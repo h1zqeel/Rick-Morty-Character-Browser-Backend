77c4173765f6a537634aa6bc379d9d7b9c8b8626
Hizqeel Javed <hizqeeljaved2013@gmail.com>
2024-07-12 03:40:12 +1000

    Formatting

--- src/data/cache.ts
+++ src/data/cache.ts
@@ -3,36 +3,40 @@ import { createClient } from 'redis';
 let cachedClient = null;
 
 const getClient = async () => {
-    if (!cachedClient && process.env.NODE_ENV !== 'test' && process.env.REDIS_URL) {
-        cachedClient = createClient({
-            url: process.env.REDIS_URL
-        });
-        await cachedClient.connect();
-    }
-    return cachedClient;
+	if (
+		!cachedClient &&
+		process.env.NODE_ENV !== 'test' &&
+		process.env.REDIS_URL
+	) {
+		cachedClient = createClient({
+			url: process.env.REDIS_URL
+		});
+		await cachedClient.connect();
+	}
+	return cachedClient;
 };
 
 const getCharactersKey = (page, filter, order, name) => {
-    return `characters:${page}:${JSON.stringify(filter)}:${order}:${name}`;
+	return `characters:${page}:${JSON.stringify(filter)}:${order}:${name}`;
 };
 
 const getCharacterKey = (id) => {
-    return `character:${id}`;
+	return `character:${id}`;
 };
 
 const createCachedData = async (key, data) => {
-    const client = await getClient();
-    if (!client) return;
+	const client = await getClient();
+	if (!client) return;
 
-    await client.set(key, JSON.stringify(data));
+	await client.set(key, JSON.stringify(data));
 };
 
 const getCachedData = async (key) => {
-    const client = await getClient();
-    if (!client) return null;
+	const client = await getClient();
+	if (!client) return null;
 
-    const data = await client.get(key);
-    return data ? JSON.parse(data) : null;
+	const data = await client.get(key);
+	return data ? JSON.parse(data) : null;
 };
 
 export { createCachedData, getCachedData, getCharactersKey, getCharacterKey };
